c6c402407e042aa60e8f5bd4c6910d80f022dd42
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2019-12-07 23:35:52 +0100

    Ajout TryCatchMain

--- Association/src/association/Main.java
+++ Association/src/association/Main.java
@@ -3,7 +3,11 @@ package association;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 	}
 
--- Association/src/modele/DepotVente.java
+++ Association/src/modele/DepotVente.java
@@ -1,6 +1,10 @@
 package modele;
 
 public class DepotVente extends Stockage {
+	
+	public DepotVente() {
+		super();
+	}
 
 	@Override
 	public String toString() {
